74330bfd6b8799c93ce8bfa00741aab001231647
Pavlo Ocheretianyi <pavel_ocheretyany_01@mail.ua>
2021-03-02 01:02:54 +0200

    Updated-6.

--- app/src/main/java/com/example/myandrexam/MainActivity.java
+++ app/src/main/java/com/example/myandrexam/MainActivity.java
@@ -5,25 +5,34 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
 {
     private EditText EditText1; // Стоимость автомобиля ($)
-    //EditText EditText2 = (EditText) findViewById(R.id.EditTextNumber_ID2); // Объем двигателя (куб. дм)
-    //Spinner Spinner1 = (Spinner) findViewById(R.id.Spinner_ID1); // Тип двигателя
-    //EditText EditText3 = (EditText) findViewById(R.id.EditTextNumber_ID3); // Год выпуска
-    //Button Button1 = (Button) findViewById(R.id.Button_ID1); // Стоимость
+    private EditText EditText2; // Объем двигателя (куб. дм)
+    private Spinner Spinner1;   // Тип двигателя
+    private EditText EditText3; // Год выпуска
+    private Button Button1;     // Стоимость
+    private TextView TextView1; // Стоимость
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         EditText1 = (EditText) findViewById(R.id.EditTextNumber_ID1);
+        EditText2 = (EditText) findViewById(R.id.EditTextNumber_ID2);
+        Spinner1 = (Spinner) findViewById(R.id.Spinner_ID1);
+        EditText3 = (EditText) findViewById(R.id.EditTextNumber_ID3);
+        Button1 = (Button) findViewById(R.id.Button_ID1);
+        TextView1 = (TextView) findViewById(R.id.TextView_ID5);
     }
 
     public void cost_button(View view)
     {
         int a1 = Integer.parseInt(EditText1.getText().toString());
+        TextView1.setText(a1);
     }
 }
