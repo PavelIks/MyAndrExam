d170f20df3519e8e98a3b5321dc0b863021d802a
Pavlo Ocheretianyi <pavel_ocheretyany_01@mail.ua>
2021-03-01 20:27:11 +0200

    Updated-4.

--- app/src/main/java/com/example/myandrexam/MainActivity.java
+++ app/src/main/java/com/example/myandrexam/MainActivity.java
@@ -1,14 +1,28 @@
 package com.example.myandrexam;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity
 {
+    EditText EditText1 = (EditText) findViewById(R.id.EditTextNumber_ID1); // Стоимость автомобиля ($)
+    //EditText EditText2 = (EditText) findViewById(R.id.EditTextNumber_ID2); // Объем двигателя (куб. дм)
+    //Spinner Spinner1 = (Spinner) findViewById(R.id.Spinner_ID1); // Тип двигателя
+    //EditText EditText3 = (EditText) findViewById(R.id.EditTextNumber_ID3); // Год выпуска
+    //Button Button1 = (Button) findViewById(R.id.Button_ID1); // Стоимость
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void Cost_Button(View view)
+    {
+        int a1 = Integer.parseInt(EditText1.getText().toString());
     }
 }
